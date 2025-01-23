95ee6acf1ad35a3f5421bec9aab61a7e567cbb8d
EslamKamel89 <elsamkamelforex@gmail.com>
2025-01-23 17:22:11 +0200

    RoleManger middleware

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::view( '/', 'welcome' );
 
 Route::view( 'dashboard', 'dashboard' )
-	->middleware( [ 'auth', 'verified' ] )
+	->middleware( [ 'auth', 'verified', 'roleManger:customer' ] )
 	->name( 'dashboard' );
 Route::view( 'admin/dashboard', 'admin' )
-	->middleware( [ 'auth', 'verified' ] )
+	->middleware( [ 'auth', 'verified', 'roleManger:admin' ] )
 	->name( 'admin' );
 Route::view( 'vendor/dashboard', 'vendor' )
-	->middleware( [ 'auth', 'verified' ] )
+	->middleware( [ 'auth', 'verified', 'roleManger:vendor' ] )
 	->name( 'vendor' );
 Route::view( 'profile', 'profile' )
 	->middleware( [ 'auth' ] )
